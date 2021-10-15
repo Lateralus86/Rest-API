ac68ce43a933217ee2f5275d1d1570be66826c79
Lateralus86 <porongo46@hotmail.com>
2021-10-15 01:09:31 -0300

    backend is like mcdonalds because im loving it

---
+++ App.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+require('dotenv/config');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+
+// const mongoose = require('mongoose');
+
+//Importing routes
+app.use(bodyParser.json());
+
+const postsRoute = require('./Routes/posts');
+app.use('/posts', postsRoute);
+
+//Middleware (the middleware is on the routes folder, just left this here)
+// app.use('/posts', () => {
+//   console.log('This is a middleware running');
+// })
+
+//Routes
+app.get('/', (req, res) => {
+  res.send('we are home');
+});
+
+app.get('/posts', (req, res) => {
+  res.send('we are on posts');
+});
+
+//connect to db
+
+const uri = process.env.DB_CONNECTION;
+mongoose.connect(uri, { useNewUrlParser: true, }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Connected to the database");
+})
+
+
+//how to start listening to the server
+app.listen(3000);
